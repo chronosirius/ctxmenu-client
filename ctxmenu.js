15d9b5e09c1db8e6a3bac232fbebe695dafabca1
chronosirius <73508925+chronosirius@users.noreply.github.com>
2023-04-27 16:12:33 -0700

    Update ctxmenu.js

--- ctxmenu.js
+++ ctxmenu.js
@@ -3,73 +3,92 @@
 
 window.addEventListener('load', function() {
     document.body.addEventListener('contextmenu', function(ev) {
-        el = document.getElementById('ctxmenu')
-        if (el) {
+        el = document.getElementById('_ctxmenu_element')
+        if (el != null) {
             ev.preventDefault()
             el.remove()
         }
     })
 })
 
-class Ctx {
-	constructor(objects) {
-		this.menu = document.createElement('div')
-		this.menu.id = 'ctxmenu'
-		this.menu.style.position = 'fixed'
-		this.menu.style.background = 'var(--bg-color)'
-		this.menu.addEventListener('contextmenu', function(ev) {
-			ev.preventDefault()
-		})
-		for (var l = 0; l < objects.length; l++) {
-			var item = objects[l]
-			var btn = document.createElement('button')
-			btn.appendChild(document.createTextNode(item.option))
-			btn.style.color = (item.color != null) ? item.color : 'var(--text-color)'
-			btn.style.background = (item.bg != null) ? item.bg : 'var(--tlv-el-color)'
-			btn.style.borderWidth = '1.5px'
-			btn.style.width = '120px'
-			btn.onclick = function() {
-				document.getElementById('ctxmenu').remove()
-				item.callback(btn)
-			}
-			btn.addEventListener('contextmenu', function(ev) {
-				ev.preventDefault()
-			})
-			btn.target = null
-			this.menu.appendChild(btn)
-			if (--objects.length) {
-				this.menu.appendChild(document.createElement('br'))
-			}
-		}
-	}
+class ContextMenuContext {
+    constructor(event, button, menu, ctx_menu_manager, query, element) {
+        this.event = event;
+        this.button = button;
+        this.menu_element = menu;
+        this.ctxmenu_manager = ctx_menu_manager;
+        this.target = event.target;
+        this.query = query;
+        this.element = element;
+    }
+}
 
-	register(query) {
-		var self = this
-		document.querySelectorAll(query).forEach(function(element) {
-			element.addEventListener('contextmenu', function(ev) {
-				ev.preventDefault()
-				ev.stopPropagation()
-				if (document.getElementById('ctxmenu') == null) {
-					var men = self.menu
-					men.style.top = `${ev.pageY}px`
-					men.style.left = `${ev.pageX}px`
-					men.style.height = 'fit-content'
-					for (var btnid = 0; btnid < men.children.length; btnid++) {
-						men.children[btnid].target = ev.target
-					}
-					document.body.appendChild(men)
-				} else {
-					document.getElementById('ctxmenu').remove()
-				}
-			})
-		})
-		document.addEventListener('click', function(e) {
-			if (document.getElementById('ctxmenu') != null) {
-				if (e.target.offsetParent != document.getElementById('ctxmenu')) {
-					document.getElementById('ctxmenu').remove()
-				}
-			}
-		})
-		return this
-	}
+class CtxMenu {
+    constructor(contextmenu_frame) {
+        var menu = document.createElement('div')
+        menu.style.position = 'fixed'
+        menu.style.background = 'var(--ctxmenu-bg-color)'
+        menu.id = '_ctxmenu_element'
+        this.registrations = [];
+        for (let custmenu of contextmenu_frame) {
+            let btn = document.createElement('button')
+            btn.appendChild(document.createTextNode(custmenu.text))
+            btn.style.color = (custmenu.color != null) ? custmenu.color : 'var(--ctxmenu-text-color)'
+			btn.style.background = (custmenu.bg != null) ? custmenu.bg : 'var(--ctxmenu-btn-bg-color)'
+            btn.style.borderWidth = '1.5px'
+			btn.style.width = '120px'
+            btn.addEventListener('contextmenu', (ev) => {ev.preventDefault()})
+            let callback = custmenu.callback
+            btn._fire_handler = function(context) {
+                document.getElementById('_ctxmenu_element').remove()
+                callback(context)
+            }
+            menu.appendChild(btn)
+            if (--contextmenu_frame.length) {
+                menu.appendChild(document.createElement('br'))
+            }
+        }
+        this.menu = menu
+    }
+
+    register(query) {
+        var self = this
+        Array.from(document.querySelectorAll(query)).forEach(function(queried_element) {
+            if (!self.registrations.includes(queried_element)) {
+                queried_element.addEventListener('contextmenu', function(ev) {
+                    ev.preventDefault()
+                    ev.stopPropagation()
+                    if (document.getElementById('_ctxmenu_element') == null) {
+                        let cm = self.menu.cloneNode(false)
+                        cm.addEventListener('contextmenu', (ev) => {ev.preventDefault()})
+                        cm.style.top = `${ev.pageY}px`
+                        cm.style.left = `${ev.pageX}px`
+                        cm.style.height = 'fit-content'
+                        for (let buttonnum = 0; buttonnum < self.menu.children.length; buttonnum++) {
+                            let button = self.menu.children[buttonnum].cloneNode(true)
+                            button._fire_handler = self.menu.children[buttonnum]._fire_handler
+                            let event_context_uf = new ContextMenuContext(ev, button, cm, self, query, queried_element)
+                            button.addEventListener('contextmenu', (ev) => {ev.preventDefault()})
+                            button.onclick = function() {
+                                button._fire_handler(event_context_uf)
+                            }
+                            cm.appendChild(button)
+                        }
+                        document.body.appendChild(cm)
+                    } else {
+                        document.getElementById('_ctxmenu_element').remove()
+                    }
+                })
+                self.registrations.push(queried_element)
+                document.addEventListener('click', function(e) {
+                    if (document.getElementById('_ctxmenu_element') != null) {
+                        if (e.target.offsetParent != document.getElementById('_ctxmenu_element')) {
+                            document.getElementById('_ctxmenu_element').remove()
+                        }
+                    }
+                })
+            }
+        })
+	return this;
+    }
 }
